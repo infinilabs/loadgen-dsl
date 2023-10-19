11fa7a1c5b363f37b90153aa6fe4146a1b2a1263
loichyan <loichyan@foxmail.com>
2023-10-19 15:02:28 +0800

    build: pin Rust toolchain to 1.65

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
@@ -8,28 +8,42 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, fenix, ... }:
+  outputs = { nixpkgs, flake-utils, ... } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlays.default ];
+          overlays = with inputs; [
+            inputs.fenix.overlays.default
+          ];
         };
+        inherit (pkgs) fenix ra-flake;
+        inherit (pkgs.lib) importTOML;
+
+        # Rust toolchain
+        rustChannel = (importTOML ./rust-toolchain).toolchain.channel;
+        rustToolchain = fenix.toolchainOf {
+          channel = rustChannel;
+          sha256 = "sha256-DzNEaW724O8/B8844tt5AVHmSjSQ3cmzlU4BP90oRlY=";
+        };
+        rustWasmToolchain = fenix.targets.wasm32-unknown-unknown.toolchainOf {
+          channel = rustChannel;
+          sha256 = "sha256-DzNEaW724O8/B8844tt5AVHmSjSQ3cmzlU4BP90oRlY=";
+        };
+        # For development
+        rust-dev = with fenix; combine (with rustToolchain; [
+          defaultToolchain
+          rustWasmToolchain.rust-std
+          rust-src
+          rust-analyzer
+        ]);
       in
       {
-        devShells.default =
-          with pkgs;
-          with pkgs.fenix;
-          mkShell {
-            nativeBuildInputs = [
-              (combine [
-                stable.defaultToolchain
-                stable.rust-src
-                targets.wasm32-unknown-unknown.stable.rust-std
-              ])
-            ];
-          };
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = [
+            rust-dev
+          ];
+        };
       }
-    )
-  ;
+    );
 }
